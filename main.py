44edd1a41006acd6e3754ce18c887ab3cb7c83cc
AnjaKroon <68068874+AnjaKroon@users.noreply.github.com>
2023-12-14 19:13:27 +0100

    final testing

--- main.py
+++ main.py
@@ -32,4 +32,11 @@ data= mat["train_eeg"]
 
 print(mat.keys())
 
+# Plotting Raw EEG
+sfreq = 400
+info = mne.create_info(ch_names=['EEG 1', 'EEG 2', 'EEG 3', 'EEG 4', 'EEG 5', 'EEG 6','EEG 7', 'EEG 8', 'EEG 9', 'EEG 10','EEG 11', 'EEG 12', 'EEG 13', 'EEG 14', 'EEG 15', 'EEG 16','EEG 17', 'EEG 18', 'EEG 19'],
+                       ch_types=['eeg'] * 19, sfreq=sfreq)
 
+raw = mne.io.RawArray(data, info)
+
+mne.viz.plot_raw(raw, duration=25.0, start=0.0, scalings=100, n_channels=19)
